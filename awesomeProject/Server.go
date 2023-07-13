35c44e0527c571d5dddf9872a543a0b0a1415953
Mobarak <mobarak@appscode.com>
2023-07-13 18:36:52 +0600

    c7:master Read 3 chapters of the book concurrency

--- awesomeProject/Server.go
+++ awesomeProject/Server.go
@@ -2,42 +2,21 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func digits(number int, dchnl chan int) {
-	for number != 0 {
-		digit := number % 10
-		dchnl <- digit
-		number /= 10
+func producer(chnl chan int) {
+	for i := 0; i < 10; i++ {
+		chnl <- i
 	}
-	close(dchnl)
+	close(chnl)
 }
-func calcSquares(number int, squareop chan int) {
-	sum := 0
-	dch := make(chan int)
-	go digits(number, dch)
-	for digit := range dch {
-		sum += digit * digit
-	}
-	squareop <- sum
-}
-
-func calcCubes(number int, cubeop chan int) {
-	sum := 0
-	dch := make(chan int)
-	go digits(number, dch)
-	for digit := range dch {
-		sum += digit * digit * digit
-	}
-	cubeop <- sum
-}
-
 func main() {
-	number := 589
-	sqrch := make(chan int)
-	cubech := make(chan int)
-	go calcSquares(number, sqrch)
-	go calcCubes(number, cubech)
-	squares, cubes := <-sqrch, <-cubech
-	fmt.Println("Final output", squares+cubes)
+	start := time.Now()
+	var c <-chan int
+	select {
+	case <-c:
+	default:
+		fmt.Printf("In default after %v\n\n", time.Since(start))
+	}
 }
